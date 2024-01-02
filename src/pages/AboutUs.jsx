1400fa2fbe096d9248cc87a79aaa199597d1ca8c
keinar <kelkayam@seatgeek.com>
2024-01-02 15:25:46 +0200

    UI updates

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,7 +3,7 @@ import { RightNav } from '../cmps/SideNav/RightNav';
 export function AboutUs() {
   return (
     <div className="flex space-between">
-      <div className="container">
+      <div className="container about-us">
         <h1>About Us</h1>
         <p>Author: Keinar Elkayam</p>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,8 @@ import logo from '../assets/imgs/logoMe.png';
 export function HomePage() {
   return (
     <div className="flex space-between">
-      <div className="container">
-        <h1 className="text-center">Welcome to Gmail</h1>
+      <div className="container home-page">
+        <h1 className="text-center">Welcome to Mister Email</h1>
         <img src={logo} alt="mister mail"></img>
       </div>
       <RightNav />
